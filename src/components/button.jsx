a4d5a39cb12b7559f57b3ee14b52bb2c680a2881
OmarAmeen01 <forcodingcoding@gmail.com>
2024-04-20 20:57:48 +0530

    made its bg transparent

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -4,7 +4,7 @@ function Button({ onClick, className, innerText }) {
   return (
     <button
       onClick={onClick}
-      className={`border border-green-500 group  p-2 rounded-xl text-lg text-[#828ead] overflow-hidden font-Mplus relative hover:text-[#c9d4f4] ${className}`}
+      className={`border border-green-500 group  p-2 rounded-xl text-lg bg-transparent text-[#828ead] overflow-hidden font-Mplus relative hover:text-[#c9d4f4] ${className}`}
     >
       <span
         className="bg-green-500 w-28 h-28 top-0 -left-40  absolute transition-all duration-200 ease-in-out
