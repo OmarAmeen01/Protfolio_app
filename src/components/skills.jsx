28ba164c280c988990bf1eba117d0030a2697241
OmarAmeen01 <forcodingcoding@gmail.com>
2024-06-26 19:40:20 +0530

    added few skills to skils section

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -8,8 +8,12 @@ import reduxIcon from "../assets/redux.svg";
 import tailwindIcon from "../assets/tailwind-css-2.svg";
 import typescriptIcon from "../assets/typescript.svg";
 import javsriptIcon from "../assets/logo-javascript.svg";
-import gsapIcon from "../assets/gsap-greensock.svg";
-
+import zod from "../assets/logos--zod.svg"
+import mysql from "../assets/mysql.svg"
+import nodejs from "../assets/nodejs.svg"
+import postgresql from "../assets/postgresql.svg"
+import cloudflare from "../assets/cloudflare-svgrepo-com.svg"
+import mongodb from "../assets/mongodb-svgrepo-com.svg";
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import "Swiper/styles";
@@ -57,10 +61,34 @@ const cards = [
     name: "Typescript",
     icon: typescriptIcon,
   },
-  {
-    name: "Gsap",
-    icon: gsapIcon,
-  },
+{
+  name:"Mongodb",
+  icon:mongodb
+},
+{
+  name:"Postgresql",
+  icon:postgresql
+},
+{
+  name:"Nodejs",
+  icon:nodejs
+},
+{
+  name:"Mongodb",
+  icon:mongodb
+},
+{
+  name:"MySQL",
+  icon:mysql
+},
+{
+  name:"Cloudflare",
+  icon:cloudflare
+},
+{
+  name:"ZOD",
+  icon:zod
+}
 ];
 function Skills() {
   const [isOpen, setOpen] = useState(false);
@@ -72,7 +100,7 @@ function Skills() {
        <p className="text-3xl max-[514px]:text-2xl max-[514px]:p-8 tracking-widest font-Mplus text-center p-14 mt-2 text-white font-extrabold  -skew-y-6  underline decoration-green-500 decoration-8 underline-offset-8">
           SKI<span className="text-green-500">LLS</span>       
              </p>
-      <article className="relative    overflow-hidden ">
+      <article className=" ">
       <Swiper
         effect={'coverflow'}
         grabCursor={true}
@@ -93,10 +121,11 @@ function Skills() {
         pagination={true}
         modules={[EffectCoverflow, Pagination]}
       
-        className="mySwiper"
+        className="mySwiper  border-2"
       >
        {cards.map(card=>{
-        return <SwiperSlide title={card.name} className="swiper-slide bg-white rounded-2xl shadow-[2px_3px_20px_5px_rgba(0,0,0,.6)] max-h-40 max-w-40"><img className="w-25 " src={card.icon} alt={card.name} />
+        return <SwiperSlide  className="swiper-slide   bg-white rounded-2xl relative shadow-[2px_3px_20px_5px_rgba(0,0,0,.6)] max-h-40 max-w-40 group">
+          <p id="title" className="absolute  p-2 bg-black text-white text-lg opacity-0  rounded-md -left-10 top-16 font-bold m-auto group-hover:opacity-100"> {card.name}</p><img className="w-25 " src={card.icon} alt={card.name} />
        </SwiperSlide>
      
        })} 
